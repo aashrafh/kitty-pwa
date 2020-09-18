cb0bbe0d0df40aef59514735443d13f452a70f95
Ahmed Ashraf <ahmed.ahmed991@eng-st.cu.edu.eg>
2020-09-18 05:27:37 +0200

    Open add cat form using js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,9 @@
 document.addEventListener("DOMContentLoaded", function() {
+  //Open the sidenav
   const sidenav = document.querySelectorAll(".side-nav");
   M.Sidenav.init(sidenav, { edge: "right" });
+
+  //add new cat
+  const form = document.querySelectorAll(".side-form");
+  M.Sidenav.init(form, { edge: "left" });
 });
