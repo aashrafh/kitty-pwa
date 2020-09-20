87d43905f06331cd0a02b245000ba6e35b81cf21
Ahmed Ashraf <ahmed.ahmed991@eng-st.cu.edu.eg>
2020-09-20 03:47:26 +0200

    Match the cached assets to fetch requests

--- serviceWorker.js
+++ serviceWorker.js
@@ -9,10 +9,10 @@ const assets = [
   "/css/materialize.min.css",
   "img/cat.png",
   "https://fonts.googleapis.com/icon?family=Material+Icons",
-  "https://fonts.gstatic.com/s/materialicons/v47/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2"
+  "https://fonts.gstatic.com/s/materialicons/v55/flUhRq6tzZclQEJ-Vdg-IuiaDsNc.woff2"
 ];
 
-//listen to the instal event
+// listen to the instal event
 self.addEventListener("install", (e) => {
   e.waitUntil(
     caches.open(staticCacheName).then((cache) => {
@@ -22,18 +22,20 @@ self.addEventListener("install", (e) => {
   );
 });
 
-//Every time you install the service worker for the first time, it becomes active automatically
-//BUT after that, you need to actice it through the active event
-self.addEventListener("activate", (e) => {
-  //   console.log("service worker has been activated");
-});
+// Every time you install the service worker for the first time, it becomes active automatically
+// BUT after that, you need to actice it through the active event
+self.addEventListener("activate", (e) => {});
 
-//The fetch event ==> when fetching something from the server (for ex. css file, js file, images)
-//or even from a js file (for example http request)
-//The fetch event listen to any fetch request and at that point you can intercept the fetch request
-//You can do nothing, modify, or stop the request
-//Why to intercept the request?
-//1- Maybe to stop the request and get the data fromt the cached assets instead of the server
+// The fetch event ==> when fetching something from the server (for ex. css file, js file, images)
+// or even from a js file (for example http request)
+// The fetch event listen to any fetch request and at that point you can intercept the fetch request
+// You can do nothing, modify, or stop the request
+// Why to intercept the request?
+// 1- Maybe to stop the request and get the data fromt the cached assets instead of the server
 self.addEventListener("fetch", (e) => {
-  //   console.log("fetch event", e);
+  e.respondWith(
+    caches.match(e.request).then((cacheResponse) => {
+      return cacheResponse || fetch(e.request); // If there is a match with a cached request then return it, otherwise do the normal fetch
+    })
+  );
 });
