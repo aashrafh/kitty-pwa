05cb7fbda73b0257b9cfdd2f8ca697ad7ba53c04
Ahmed Ashraf <ahmed.ahmed991@eng-st.cu.edu.eg>
2020-09-20 02:50:38 +0200

    Listen to the fetch event

--- serviceWorker.js
+++ serviceWorker.js
@@ -8,3 +8,13 @@ self.addEventListener("install", (e) => {
 self.addEventListener("activate", (e) => {
   console.log("service worker has been activated");
 });
+
+//The fetch event ==> when fetching something from the server (for ex. css file, js file, images)
+//or even from a js file (for example http request)
+//The fetch event listen to any fetch request and at that point you can intercept the fetch request
+//You can do nothing, modify, or stop the request
+//Why to intercept the request?
+//1- Maybe to stop the request and get the data fromt the cached assets instead of the server
+self.addEventListener("fetch", (e) => {
+  console.log("fetch event", e);
+});
