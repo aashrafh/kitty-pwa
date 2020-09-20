0b2ba57df821b8e9bdfc8ae4da6e0957c259ed96
Ahmed Ashraf <ahmed.ahmed991@eng-st.cu.edu.eg>
2020-09-20 02:31:23 +0200

    Audit Lighthouse

--- serviceWorker.js
+++ serviceWorker.js
@@ -2,3 +2,9 @@
 self.addEventListener("install", (e) => {
   console.log("service worker has been installed");
 });
+
+//Every time you install the service worker for the first time, it becomes active automatically
+//BUT after that, you need to actice it through the active event
+self.addEventListener("activate", (e) => {
+  console.log("service worker has been activated");
+});
